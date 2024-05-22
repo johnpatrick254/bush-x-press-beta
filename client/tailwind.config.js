3d189fe92750cfe2cb3858ab85472730623e4724
johnpatrick254 <jpattrick538@gmail.com>
2024-05-22 18:36:54 +0300

    fix: updated tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/**/*.{js,jsx,ts,tsx}", "./components/**/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+     colors: {
+      primary: '#FDFDFD',
+      secondary: '#1E1F1D',
+      tertiary: '#F0F0F0',
+     },
+     spacing:{
+      xm:'4px',
+      sm:'8px',
+      md:'16px',
+      lg:'34px',
+      xxl:'34px'
+     }
   },
   plugins: [],
 }
-
+}
