60a8dbf718ef573d31248d1c35655ff82b38c46a
Solomon Udofia <meetsolomonudofia@gmail.com>
2024-05-22 22:27:17 +0100

    implemented RNRC in the home page

--- client/components/dashboard/browse.dashboard.tsx
+++ client/components/dashboard/browse.dashboard.tsx
@@ -1,18 +1,34 @@
 import { FontAwesome6 } from "@expo/vector-icons";
-import { Text, View } from "react-native";
+import { Dimensions, Text, View } from 'react-native';
+import Carousel from 'react-native-reanimated-carousel';
 
 
 export default function ProductBrowser() {
+  const width = Dimensions.get('window').width
   return (
-    <View className="w-full h-60 ">
+    <View className="w-full">
       
       <View className="flex flex-row w-full justify-between items-center h-max my-md p-xm">
         <Text className="text-2xl font-semibold">Browse Collections</Text>
         <FontAwesome6 size={28} name="heart" />
       </View>
 
-      <View className=" flex h-full justify-between items-center bg-red-400 py-xm">
-        <Text></Text>
+      <View className="justify-between items-center py-xm">
+        <Carousel
+                loop
+                width={width}
+                height={width / 2}
+                autoPlay={true}
+                data={[...new Array(6).keys()]}
+                scrollAnimationDuration={2000}
+                renderItem={({ index }) => (
+                    <View className="bg-red-400 flex-1 justify-center items-center mx-3 rounded-3xl">
+                        <Text style={{ textAlign: 'center', fontSize: 30 }}>
+                            {index}
+                        </Text>
+                    </View>
+                )}
+            />
       </View>
 
     </View>
