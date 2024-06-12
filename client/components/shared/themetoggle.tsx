60732801ef53b4419e7de77b054503f28c383996
johnpatrick254 <jpattrick538@gmail.com>
2024-06-12 11:17:01 +0300

    style: update theme toggle styles

--- client/components/shared/themetoggle.tsx
+++ client/components/shared/themetoggle.tsx
@@ -3,19 +3,18 @@ import { useColorScheme } from "nativewind";
 import { TouchableOpacity } from 'react-native';
 import { View } from "@/components/Themed";
 import Feather from '@expo/vector-icons/Feather';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function ThemeToggle() {
     const { colorScheme, toggleColorScheme } = useColorScheme();
 
     return (
         <View
-            className="block rounded-sm border-3 border-foreground ring-1 m-5 w-20 h-20 p-4 bg-blue-700"
+            className="flex justify-center items-center border-3 border-foreground ring-1 p-2  bg-background rounded-md"
         >
             <TouchableOpacity
                 onPress={toggleColorScheme}
             >
-                {colorScheme === "light" ? <MaterialCommunityIcons name="moon-waxing-crescent" size={35} /> : <Feather name="sun" color={"white"} size={35} />}
+                {colorScheme === "light" ? <Feather name="moon"  size={40} /> : <Feather name="sun" color={"white"} size={40} />}
             </TouchableOpacity>
         </View>
     );
